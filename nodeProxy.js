eb6da7932abd241ac02fe04991e7dabdd0c9637e
Serg <serj.nights@live.com>
2014-06-25 19:39:25 -0700

    updated to work with http-proxy 1.0.0

--- nodeProxy.js
+++ nodeProxy.js
@@ -1,10 +1,15 @@
 var http = require('http');
 var httpProxy = require('http-proxy');
 
-httpProxy.createServer({
-    hostnameOnly: true,
-    router: {
-        'www.logist36.ru': '127.0.0.1:3001',
-        'www.new.lanks.net' : '127.0.0.1:3002'
-    }
+var proxy = httpProxy.createProxy();
+
+var options = {
+    'logist36.ru': '127.0.0.1:3000',
+    'new.lanks.net': '127.0.0.1:4000'
+};
+
+http.createServer(function(req, res) {
+    proxy.web(req, res, {
+        target: options[req.headers.host]
+    });
 }).listen(80);
