1c78010c551ef61fe5adf109a4b9407d1782901f
Serg <serj.nights@live.com>
2014-07-22 20:39:02 -0700

    Numerous little tweaks

--- public/javascripts/online.js
+++ public/javascripts/online.js
@@ -14,48 +14,9 @@ $(document).ready(function() {
     }
 
     $("#track_button").click(function(){
-        var id = $("#track_id").val();
-        $.post(
-            "wsgi/.cargo_script.wsgi",
-            {
-                "id": id
-            },
-            onAjaxSuccess
-        );
-
-        function onAjaxSuccess(data)
-        {
-            // Здесь мы получаем данные, отправленные сервером и выводим их на экран.
-			if (data != "") {
-				var split_data = data.split("&");
-				$("#section4 div").remove();
-				$("#section4").append('<div><strong>Отправлено из: </strong>'+(split_data[1].split(":"))[1]+'</div>');
-				$("#section4").append('<div><strong>Дата отправления: </strong>'+(split_data[3].split(":"))[1]+'</div>');
-				$("#section4").append('<div><strong>Отправлено в: </strong>'+(split_data[2].split(":"))[1]+'</div>');
-				$("#section4").append('<div><strong>Ориентировочная дата прибытия: </strong>'+(split_data[4].split(":"))[1]+'</div>');
-				switch ((split_data[0].split(":"))[1]){
-					case "in_way": {
-						$("#section4").append('<div><strong>Статус груза: </strong>В пути.</div>');
-						break;
-					}
-					case "on_stock": {
-						$("#section4").append('<div><strong>Статус груза: </strong>На складе.</div>');
-						break;
-					}
-					case "arrived": {
-						$("#section4").append('<div><strong>Статус груза: </strong>Прибыл.</div>');
-						break;
-					}
-	
-				}
-			} else {
-				$("#section4 div").remove();
-				$("#section4").append('<div><strong>Груза с таким номером не найдено.</div>');
-				$("#section4").append('<div><strong>Пожалуйста, проверьте номер груза или свяжитесь c оператором.</div>');
-			}
-
-        }
-
+        $("#section4 div").remove();
+        $("#section4").append('<div><strong>Груз с таким номером не внесен в систему.</div>');
+        $("#section4").append('<div><strong>Пожалуйста, проверьте номер груза или <a href="/question">свяжитесь c оператором</a>.</div>');
     });
 	$("#track_orif_button").click(function(){
         var id = $("#track_orif_id").val();
@@ -81,7 +42,6 @@ $(document).ready(function() {
                     $("#section6").append('<div><strong>Заказа с таким номером не найдено.</div>');
                     $("#section6").append('<div><strong>Пожалуйста, проверьте номер заказа или свяжитесь c оператором.</div>');
                 }
-
             }
         );
 
--- routes/departments.js
+++ routes/departments.js
@@ -1,12 +1,24 @@
 var Departments = require('../modelsDB/departments').Departments;
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 router.get('/', function(req, res) {
     if (req.query.city){
         Departments.findOne({cityEng: req.query.city}, function(err, departments){
             if(err) throw err;
-            res.render('departments', {mode: 'single',allDepartments: departments});
+            fs.exists('views/include/'+req.query.city+'.hjs', function(exists) {
+                if (err) throw err;
+                if (exists) {
+                    res.render('include/'+req.query.city, function(err, additionalInfo){
+                        if (err) throw err;
+                        req.additionalInfo = res.locals.additionalInfo = additionalInfo;
+                        res.render('departments', {mode: 'single',allDepartments: departments});
+                    });
+                } else {
+                    res.render('departments', {mode: 'single',allDepartments: departments});
+                }
+            });
         });
     } else {
         Departments.find({}, function(err, departments){
