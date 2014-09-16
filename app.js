055ccb109f69d326220feba0fc380d3cb7ba1161
Serg <serj.nights@live.com>
2014-09-15 18:39:12 -0700

    favicon console log removed

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ var bodyParser = require('body-parser');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
-console.log(__dirname + '/public/favicon.ico');
 app.use(favicon(__dirname + '/public/favicon.png'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
