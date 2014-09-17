409309574f38229f0a5c01fa26e0117360f2dd82
Serg <serj.nights@live.com>
2014-09-16 21:37:24 -0700

    brand new gallery based on vk.com API

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -1,4 +1,9 @@
-$(document).ready(function()
-{
-
+$(document).ready(function(){
+    $('.albumCaption').on('click', function(event){
+        var albumId = $(this).data("id");
+        $('.albumContent:visible').hide();
+        $('.albumContent').filter(function() {
+            return $(this).data("id") == albumId
+        }).toggle();
+    })
 });
--- routes/gallery.js
+++ routes/gallery.js
@@ -1,164 +1,62 @@
-/*var GalleryAlbums = require('../modelsDB/galleryAlbums').GalleryAlbums;*/
 var express = require('express');
 var router = express.Router();
+var http = require('http');
+var async = require('async');
 
-/*var formidable = require('formidable');
-var util = require('util');
-var fs = require('fs');*/
-
-/*router.get('/json', function(req, res) {
-    if (req.query.galleryAlbumID){
-        GalleryAlbums.find({_id: req.query.galleryAlbumID}, function(err, galleryAlbum){
-            if(err) throw err;
-            res.send(galleryAlbum);
+function getPhotos (aid, callback) {
+    http.get("http://api.vk.com/method/photos.get?gid=35203647&aid="+aid, function(vkPhotosRes) {
+        var rawPhotos = '';
+        vkPhotosRes.on("data", function(photoChunk) {
+            rawPhotos += photoChunk;
         });
-    } else {
-        GalleryAlbums.find({}, function(err, galleryAlbums){
-            if(err) throw err;
-            galleryAlbums.reverse();
-            galleryAlbums.sort(function(a,b){
-                if (!a.index){
-                    return 1;
-                } else {
-                    if(!b.index){
-                        return -1;
-                    } else {
-                        return b.index - a.index;
-                    }
+        vkPhotosRes.on('end', function() {
+            if (rawPhotos !== '') {
+                rawPhotos = JSON.parse(rawPhotos).response;
+                var albumPictures = [];
+                for (var j=0; j<rawPhotos.length; j++){
+                    albumPictures.push(rawPhotos[j].src_big);
                 }
-            });
-            res.send(galleryAlbums);
+
+                callback(null, albumPictures);
+            }
         });
-    }
-});*/
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+    });
+}
 
 router.get('/', function(req, res) {
 
-    res.render('gallery', {});
-   /* if (req.query.galleryAlbumID){
-        if (req.query.galleryAlbumID !== '-1'){
-            GalleryAlbums.find({_id: req.query.galleryAlbumID}, function(err, galleryAlbum){
-                if(err) throw err;
-                res.render('gallery', {mode: 'singleAlbum', singleAlbum: galleryAlbum});
-            });
-        } else {
-            res.render('gallery', {mode: 'singleAlbum', singleAlbum: {}});
-        }
-
-    } else {
-        GalleryAlbums.find({}, function(err, galleryAlbums){
-            if(err) throw err;
-            galleryAlbums.reverse();
-            galleryAlbums.sort(function(a,b){
-                if (!a.index){
-                    return 1;
-                } else {
-                    if(!b.index){
-                        return -1;
-                    } else {
-                        return b.index - a.index;
-                    }
-                }
-            });
-            res.render('gallery', {allAlbums: galleryAlbums});
+    http.get("http://api.vk.com/method/photos.getAlbums?need_covers=1&gid=35203647", function(vkRes) {
+        var rawAlbums = '';
+        var albums = [];
+        var aids = [];
+        vkRes.on("data", function(chunk) {
+            rawAlbums += chunk;
         });
-    }*/
-});
-//post is not ready
-/*router.post('/', function(req, res) {
-    var form = new formidable.IncomingForm();
-    form.encoding = 'utf-8';
-    form.uploadDir = "./temp";
-    form.keepExtensions = true;
-    form.parse(req, function(err, fields, files) {
-        if (err) throw err;
-
-        if (files.logo.name){
-            fs.rename(files.logo.path, "./public/images/news/" + fields._id + '.' + files.logo.type.split('/')[1], function(err){
-                if (err) throw err;
-            });
-        } else {
-            fs.unlink(files.logo.path, function(err){
-                if (err) throw err;
-            });
-        }
-
-        var galleryAlbum = new GalleryAlbums (
-            { index:  fields.index,
-                albumName: fields.albumName,
-                albumDescription: fields.albumDescription,
-                albumCover: fields.albumCover,
-                albumPictures: []
-            }*//*
-
-            { title:  fields.title,
-            logo: fields._id + '.' + files.logo.type.split('/')[1],
-            shortDescription:  fields.shortDescription,
-            description:  fields.description,
-            fullText: fields.fullText,
-            date: fields.date,
-            index: fields.index
-        }*//*);
-        galleryAlbum.save(function(err){
-            if (err) throw err;
-            res.writeHead(200, {'content-type': 'text/plain'});
-            res.write('received upload:\n\n');
-            res.end(util.inspect({fields: fields, files: files}));
-        })
-    });
-});*/
-
-/*router.put('/', function(req, res) {
-    var form = new formidable.IncomingForm();
-    form.encoding = 'utf-8';
-    form.uploadDir = "./temp";
-    form.keepExtensions = true;
-    form.parse(req, function(err, fields, files) {
-        if (err) throw err;
-        if (files.logo.name){
-            fs.rename(files.logo.path, "./public/images/news/" + fields._id + '.' + files.logo.type.split('/')[1], function(err){
-                if (err) throw err;
-            });
-        } else {
-            fs.unlink(files.logo.path, function(err){
-                if (err) throw err;
-            });
-        }
-        News.findById(fields._id, function(err, news){
-            if(err) throw err;
-            if (news) {
-                news.logo = fields._id + '.' + files.logo.type.split('/')[1];
-                for (var i in fields){
-                    news[i] = fields[i];
-                }
-                news.save(function(err){
-                    if (err) throw err;
-                    res.writeHead(200, {'content-type': 'text/plain'});
-                    res.write('received upload:\n\n');
-                    res.end(util.inspect({fields: fields, files: files}));
-                })
+        vkRes.on('end', function() {
+            rawAlbums = JSON.parse(rawAlbums).response;
+            for (var i=0; i<rawAlbums.length; i++){
+                aids.push(rawAlbums[i].aid);
+                albums.push({albumTitle: rawAlbums[i].title, albumCover: rawAlbums[i].thumb_src, albumId: rawAlbums[i].aid});
             }
-        });
-    });
-});
 
-router.delete('/', function(req, res) {
-    News.findById(req.body.id, function (err, news) {
-        if (err) throw err;
-        if(!news) {
-            res.statusCode = 404;
-            res.send({ error: 'Not found' });
-        } else {
-            news.remove(function (err) {
-                if (!err) {
-                    res.send({ status: 'OK' });
+            async.map(aids, getPhotos, function(err, results){
+                if( err ) {
+                    console.log('Failed to process');
                 } else {
-                    res.statusCode = 500;
-                    res.send({ error: 'Server error' });
+                    for (var i=0; i<results.length; i++){
+                        albums[i].albumPictures = results[i];
+                    }
+                    res.render('gallery', {allAlbums: albums});
                 }
             });
-        }
+        });
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
     });
-});*/
-
+    // https://api.vk.com/method/photos.getAlbums?gid=35203647
+    // https://api.vk.com/method/photos.get?gid=35203647&aid=200974910
+    //https://vk.com/pages?oid=-1&p=%D0%9E%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D0%B5_%D0%BC%D0%B5%D1%82%D0%BE%D0%B4%D0%BE%D0%B2_API
+});
 module.exports = router;
